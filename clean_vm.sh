979ae924ac002ffbc25b974c758c788237780dcc
João Vitor <joaovytor0@gmail.com>
2019-05-15 21:36:39 -0300

    Update configs

--- clean_vm.sh
+++ clean_vm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-USERNAME=${USERNAME}
-HOSTS=${HOSTS}
-SCRIPT=${SCRIPT}
-ID_RSA=${ID_RSA}
+USERNAME=teste
+HOSTS=111.111.11.111
+SCRIPT="docker system prune -a -f"
+ID_RSA=/path/to/id_rsa
 for HOSTNAME in ${HOSTS} ; do
     ssh -i ${ID_RSA} -l ${USERNAME} ${HOSTNAME} "${SCRIPT}"
 done
--- export.sh
+++ export.sh
@@ -1,4 +1,4 @@
-export USERNAME=your_username
+export USERNAME=teste
 export HOSTS=111.111.11.111
 export SCRIPT="docker system prune -a -f"
-export ID_RSA=your_id_rsa
+export ID_RSA=seu_id_rsa
